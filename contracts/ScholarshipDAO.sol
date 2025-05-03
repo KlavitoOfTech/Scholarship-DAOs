d186c09e12d6ec3693d76727f1d2ce9f8b71a1d4
KlavitoOfTech <ejibeclaver0@gmail.com>
2025-05-03 12:18:43 +0100

    fix: added the results function to access vote winners.

--- contracts/ScholarshipDAO.sol
+++ contracts/ScholarshipDAO.sol
@@ -29,21 +29,27 @@ contract ScholarshipDAO {
         voters[_voter] = true;
     }
 
-    function createProposal(string memory _name, string memory _description, uint256 _amount, address payable _recipient) external onlyAdmin {
-        proposals.push(Proposal({
-            name: _name,
-            description: _description,
-            amount: _amount,
-            recipient: _recipient,
-            votes: 0,
-            executed: false
-        }));
+    function createProposal(
+        string memory _name,
+        string memory _description,
+        uint256 _amount,
+        address payable _recipient
+    ) external onlyAdmin {
+        proposals.push(
+            Proposal({
+                name: _name,
+                description: _description,
+                amount: _amount,
+                recipient: _recipient,
+                votes: 0,
+                executed: false
+            })
+        );
     }
 
     function vote(uint256 _proposalId) external {
         require(voters[msg.sender], "Not a registered voter");
         require(!proposals[_proposalId].executed, "Already executed");
-
         proposals[_proposalId].votes++;
     }
 
@@ -52,7 +58,6 @@ contract ScholarshipDAO {
         require(!proposal.executed, "Already executed");
 
         proposal.executed = true;
-        
         uint256 contractBalance = address(this).balance;
         require(contractBalance >= proposal.amount, "Not enough funds");
 
@@ -60,8 +65,39 @@ contract ScholarshipDAO {
         require(success, "Transfer failed");
     }
 
-    function proposalsLength() external view returns (uint) {
+    function proposalsLength() external view returns (uint256) {
         return proposals.length;
+    }
+
+    function getWinningProposal() public view returns (uint256) {
+        uint256 winningVoteCount = 0;
+        uint256 winningProposalId = 0;
+
+        for (uint256 i = 0; i < proposals.length; i++) {
+          if (proposals[i].votes > winningVoteCount) {
+              winningVoteCount = proposals[i].votes;
+              winningProposalId = i;
+          }
+        }
+        return winningProposalId;
+    }
+
+
+    function finalizeWinner() external onlyAdmin {
+        require(proposals.length > 0, "No proposals available");
+
+        uint256 winningProposalId = getWinningProposal();
+        Proposal storage winningProposal = proposals[winningProposalId];
+
+        require(!winningProposal.executed, "Already executed");
+
+        winningProposal.executed = true;
+
+        uint256 contractBalance = address(this).balance;
+        require(contractBalance >= winningProposal.amount, "Not enough funds");
+
+        (bool success, ) = winningProposal.recipient.call{value: winningProposal.amount}("");
+        require(success, "Winner transfer failed");
     }
 
     receive() external payable {}
